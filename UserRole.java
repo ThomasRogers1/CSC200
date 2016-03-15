5ddfe590ae5238e34b57432958248fccfaad7da3
ThomasRogers1 <trogers5144@gmail.com>
2016-03-15 19:42:14 -0400

    Java Code using switch case.

---
+++ UserRole.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class UserRole {
+	public static void main(String [] args) {
+		String Role;
+		Role = JOptionPane.showInputDialog("Enter Role" + "\n" + "(Administrator, Faculty, Staff, Student, Guest)");
+
+		switch(Role) {
+		case "Administrator":
+			JOptionPane.showMessageDialog(null, "Welcome Administrator!");
+			break;
+
+		case "Faculty":
+			JOptionPane.showMessageDialog(null, "Welcome Faculty!");
+			break;
+
+		case "Staff":
+			JOptionPane.showMessageDialog(null, "Welcome Staff!");
+			break;
+	
+		case "Student":
+			JOptionPane.showMessageDialog(null, "Welcome Student!");
+			break;
+
+		case "Guest":
+			JOptionPane.showMessageDialog(null, "Welcome Student!:");
+			break;
+
+		default:
+			JOptionPane.showMessageDialog(null, "Your role is incorrect!");
+		}
+	}
+}
